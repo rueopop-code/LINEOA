25b929e370cbdc094b9e2f52e351565a55e221a8
PHONE <rueopop@gmail.com>
2026-06-10 12:20:11 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2814,10 +2814,16 @@ app.get('/invite/:refCode', (req, res) => {
 app.use(express.static(__dirname));
 
 app.get('/shop', (req, res) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/', (req, res) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
